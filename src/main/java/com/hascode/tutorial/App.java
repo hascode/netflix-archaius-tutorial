89810d4734c40c82cc7d32483532d55d5219e79f
Micha Kops <micha.kops@gmail.com>
2016-04-03 22:38:42 +0200

    System property configuration added, Zookeeper example fixed.

--- src/main/java/com/hascode/tutorial/App.java
+++ src/main/java/com/hascode/tutorial/App.java
@@ -1,16 +1,13 @@
 package com.hascode.tutorial;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.SystemConfiguration;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.server.ServerConfig;
-import org.apache.zookeeper.server.ZooKeeperServerMain;
-import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+import org.apache.curator.retry.RetryOneTime;
 
+import com.google.common.io.Files;
 import com.netflix.config.ConcurrentCompositeConfiguration;
 import com.netflix.config.ConcurrentMapConfiguration;
 import com.netflix.config.ConfigurationManager;
@@ -18,72 +15,54 @@ import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
 import com.netflix.config.DynamicWatchedConfiguration;
 import com.netflix.config.source.ZooKeeperConfigurationSource;
+import com.netflix.curator.test.TestingServer;
 
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		startZookeeperForDemonstration();
+		try (TestingServer zkTestServer = new TestingServer(2181, Files.createTempDir());
+				CuratorFramework testClient = CuratorFrameworkFactory.newClient(zkTestServer.getConnectString(),
+						new RetryOneTime(2000))) {
+			testClient.start();
+			testClient.create().forPath("/config");
+			testClient.create().forPath("/config/search.url", "http://www.test.de".getBytes());
 
-		// enable JMX dynamic configuration
-		System.setProperty(DynamicPropertyFactory.ENABLE_JMX, "true");
+			// enable JMX dynamic configuration
+			System.setProperty(DynamicPropertyFactory.ENABLE_JMX, "true");
 
-		// enable config from properties file
-		ConcurrentMapConfiguration configFile = new ConcurrentMapConfiguration(
-				new PropertiesConfiguration("config.properties"));
+			// enable config from properties file
+			ConcurrentMapConfiguration configFile = new ConcurrentMapConfiguration(
+					new PropertiesConfiguration("config.properties"));
 
-		String zkConfigRootPath = "/[my-app]/config";
+			// enable configuration from system properties
+			ConcurrentMapConfiguration systemProps = new ConcurrentMapConfiguration(new SystemConfiguration());
 
-		CuratorFramework client = CuratorFrameworkFactory.newClient("", new ExponentialBackoffRetry(1000, 3));
+			CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181",
+					new ExponentialBackoffRetry(1000, 3));
+			client.start();
 
-		ZooKeeperConfigurationSource zkConfigSource = new ZooKeeperConfigurationSource(client, zkConfigRootPath);
-		zkConfigSource.start();
+			ZooKeeperConfigurationSource zkConfigSource = new ZooKeeperConfigurationSource(client, "/config");
+			zkConfigSource.start();
+			System.out.println("value in zookeeper: " + zkConfigSource.getCurrentData().get("search.url"));
+			DynamicWatchedConfiguration zkDynamicConfig = new DynamicWatchedConfiguration(zkConfigSource);
 
-		DynamicWatchedConfiguration zkDynamicConfig = new DynamicWatchedConfiguration(zkConfigSource);
+			// composite configuration
+			ConcurrentCompositeConfiguration config = new ConcurrentCompositeConfiguration();
+			config.addConfiguration(zkDynamicConfig);
+			config.addConfiguration(configFile);
+			config.addConfiguration(systemProps);
+			ConfigurationManager.install(config);
 
-		// composite configuration
-		ConcurrentCompositeConfiguration config = new ConcurrentCompositeConfiguration();
-		config.addConfiguration(configFile);
-		config.addConfiguration(zkDynamicConfig);
-		ConfigurationManager.install(config);
+			DynamicStringProperty prop = DynamicPropertyFactory.getInstance().getStringProperty("search.url",
+					"https://www.google.com/#q=");
 
-		DynamicStringProperty prop = DynamicPropertyFactory.getInstance().getStringProperty("search.url",
-				"https://www.google.com/#q=");
+			prop.addCallback(() -> System.out.println("search url has changed to " + prop.get()));
 
-		prop.addCallback(() -> System.out.println("search url has changed to " + prop.get()));
+			System.out.println("search url is: " + prop.get());
 
-		System.out.println("search url is: " + prop.get());
-
-		for (;;) {
+			testClient.setData().forPath("/config/search.url", "http://www.foo.de".getBytes());
+			client.close();
 		}
-	}
-
-	private static void startZookeeperForDemonstration() {
-		Properties zkConfig = new Properties();
-		zkConfig.setProperty("tickTime", "2000");
-		zkConfig.setProperty("dataDir", "/tmp/zookeeper");
-		zkConfig.setProperty("clientPort", "2181");
-
-		QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
-		try {
-			quorumConfiguration.parseProperties(zkConfig);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-		ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
-		final ServerConfig configuration = new ServerConfig();
-		configuration.readFrom(quorumConfiguration);
-
-		new Thread() {
-			@Override
-			public void run() {
-				try {
-					zooKeeperServer.runFromConfig(configuration);
-				} catch (IOException e) {
-					System.err.println("zookeeper failed " + e.getMessage());
-				}
-			}
-		}.start();
 	}
 
 }
